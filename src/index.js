f21b146760252fc4e6421e7044b32382def6a139
AngelRs12 <162938457+AngelRs12@users.noreply.github.com>
2024-12-09 23:39:01 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ const app = require('./app');
 
 const PORT = 3000;
 
-// Define las rutas antes de iniciar el servidor
-app.get('/', (req, res) => {
-    res.redirect('/productos'); // Redirige a la ruta /productos
-});
-
 // Inicia el servidor
 app.listen(PORT, () => {
     console.log(`🚀 Servidor escuchando en: http://localhost:${PORT}`);
