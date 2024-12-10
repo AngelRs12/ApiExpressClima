09393e485f0fd0c3c3259651521ce891e6383312
AngelRs12 <162938457+AngelRs12@users.noreply.github.com>
2024-12-10 00:02:25 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const app = require('./app');
+const PORT = process.env.PORT || 3000;
 
-const PORT = 3000;
+// Define la redirección en la ruta raíz antes de iniciar el servidor
+app.get('/', (req, res) => {
+    res.redirect('/ciudad'); // Redirige a la ruta /ciudad
+});
 
 // Inicia el servidor
 app.listen(PORT, () => {
